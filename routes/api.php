431c5b30743b9ba9de688ce5631aee7bf7fd43b8
Isurxd <suryatripatih@gmail.com>
2024-08-02 08:06:21 +0700

    baru

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,10 @@ Route::get('/user', function (Request $request) {
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::resource('liga', LigaController::class)->only(['index', 'store', 'update', 'destroy']);
+    Route::resource('liga', LigaController::class)->except(['create', 'edit']);
+    Route::resource('klub', KlubController::class)->except(['create', 'edit']);
+    Route::resource('pemain', PemainController::class)->except(['create', 'edit']);
+    Route::resource('fan', FanController::class)->except(['create', 'edit']);
 });
 
 Route::post('login', [AuthController::class, 'login']);
@@ -26,7 +30,3 @@ Route::post('register', [AuthController::class, 'register']);
 // Route::get('liga/{id}',[LigaController::class,'show']);
 // Route::put('liga/{id}',[LigaController::class,'update']);
 // Route::delete('liga/{id}',[LigaController::class,'destroy']);
-Route::resource('liga', LigaController::class)->except(['create', 'edit']);
-Route::resource('klub', KlubController::class)->except(['create', 'edit']);
-Route::resource('pemain', PemainController::class)->except(['create', 'edit']);
-Route::resource('fan', FanController::class)->except(['create', 'edit']);
